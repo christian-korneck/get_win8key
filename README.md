e29afb1f7a5956e0446a86a0569f29795fb8bd31
Christian Korneck <christian@korneck.de>
2014-01-04 23:51:59 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 get_win8key
 ===========
 
-script to read the Windows 8.x OEM license key from PC firmware
+script to read a preinstalled Windows 8.x OEM license key from a PC's firmware
+
+
+Description
+-------------
+PCs with a preinstalled Windows 8.x OEM version don't seem to ship with any printed license record, CD Key, etc.
+Instead it looks like that the PC's individual license has been saved in the device's firmware (ACPI). When the user tries to reinstall Windows, the Windows Setup reads the license key from the firmware memory, so there's no need to manually type in the actual Windows key.
+
+To still be able to backup / inventorize the license key, this script tries to read the Windows 8.x key from the PC firmware.
+(from ACPI -> MSDM table -> byte offset 56 to end)
+
+
+Usage
+-------------
+run "get_win8key.exe" or "python get_win8key.py" from a Windows shell.
+
+Requirements
+-------------
+-Windows Vista or higher (32 or 64 bit)
+-tested with Python 2.7
+
+Files
+-------------
+get_win8key.py 			the script
+get_win8key.exe			compiled py2exe
---
+++ get_win8key.py
@@ -0,0 +1,74 @@
+import ctypes
+import ctypes.wintypes
+
+#####################################################
+#script to query windows 8.x OEM key from PC firmware
+#ACPI -> table MSDM -> raw content -> byte offset 56 to end
+#ck, 03-Jan-2014 (christian@korneck.de)
+#####################################################
+
+#for ref: common STR to DWORD conversions: ACPI: 1094930505 - FIRM: 1179210317 - RSMB: 1381190978 - FACP: 1178682192 - PCAF: 1346584902 - MSDM: 1297302605 - MDSM  1296323405
+
+def EnumAcpiTables():
+#returns a list of the names of the ACPI tables on this system
+	FirmwareTableProviderSignature=ctypes.wintypes.DWORD(1094930505)
+	pFirmwareTableBuffer=ctypes.create_string_buffer(0)
+	BufferSize=ctypes.wintypes.DWORD(0)
+	#http://msdn.microsoft.com/en-us/library/windows/desktop/ms724259
+	EnumSystemFirmwareTables=ctypes.WinDLL("Kernel32").EnumSystemFirmwareTables
+	ret=EnumSystemFirmwareTables(FirmwareTableProviderSignature, pFirmwareTableBuffer, BufferSize)
+	pFirmwareTableBuffer=None
+	pFirmwareTableBuffer=ctypes.create_string_buffer(ret)
+	BufferSize.value=ret
+	ret2=EnumSystemFirmwareTables(FirmwareTableProviderSignature, pFirmwareTableBuffer, BufferSize)
+	return [pFirmwareTableBuffer.value[i:i+4] for i in range(0, len(pFirmwareTableBuffer.value), 4)]
+
+def FindAcpiTable(table):
+#checks if specific ACPI table exists and returns True/False
+	tables = EnumAcpiTables()
+	if str(table) in tables:
+		return True
+	else:
+		return False
+
+def GetAcpiTable(table,TableDwordID):
+#returns raw contents of ACPI table
+	#http://msdn.microsoft.com/en-us/library/windows/desktop/ms724379x
+	GetSystemFirmwareTable=ctypes.WinDLL("Kernel32").GetSystemFirmwareTable
+	FirmwareTableProviderSignature=ctypes.wintypes.DWORD(1094930505)
+	FirmwareTableID=ctypes.wintypes.DWORD(int(TableDwordID))
+	pFirmwareTableBuffer=ctypes.create_string_buffer(0)
+	BufferSize=ctypes.wintypes.DWORD(0)
+	ret = GetSystemFirmwareTable(FirmwareTableProviderSignature, FirmwareTableID, pFirmwareTableBuffer, BufferSize)
+	pFirmwareTableBuffer=None
+	pFirmwareTableBuffer=ctypes.create_string_buffer(ret)
+	BufferSize.value=ret
+	ret2 = GetSystemFirmwareTable(FirmwareTableProviderSignature, FirmwareTableID, pFirmwareTableBuffer, BufferSize)
+	return pFirmwareTableBuffer.raw
+	
+def GetWindowsKey():
+	#returns Windows Key as string
+	table="MSDM"
+	TableDwordID=1296323405
+	if FindAcpiTable(table)==True:
+		try:
+			rawtable = GetAcpiTable(table, TableDwordID)
+			#http://msdn.microsoft.com/library/windows/hardware/hh673514
+			#byte offset 36 from beginning = Microsoft 'software licensing data structure' / 36 + 20 bytes offset from beginning = Win Key
+			return str(rawtable[56:len(rawtable)])
+		except:
+			return False
+	else:
+		print "[ERR] - ACPI table " + table + " not found on this system"
+		return False
+	
+try:	
+	WindowsKey=GetWindowsKey()
+	if WindowsKey==False:
+		print "unexpected error"
+		exit(1)
+	else:
+		print str(WindowsKey)
+except:
+	print "unexpected error"
+	exit(1)
